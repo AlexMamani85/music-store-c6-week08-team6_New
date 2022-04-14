f586853b3fc8caa06f2e7c529fc35e098a0e22c2
Enmanuel <emachipana0102@gmail.com>
2022-04-14 00:08:12 -0500

    ADD: validations for artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,21 @@
 class Artist < ApplicationRecord
+  validates :name, presence: true
+  validate :date_cant_be_in_the_future
+  validate :birth_date_non_greater_than_death_date
+  validate :birth_date_exists?
+
+  private
+
+  def date_cant_be_in_the_future
+    errors.add(:birth_date, "can't be in the future") if birth_date.present? && birth_date > Date.current
+  end
+
+  def birth_date_non_greater_than_death_date
+    errors.add(:death_date,
+         "invalid death date must be greater than birth date") if birth_date.present? && death_date.present? && birth_date > death_date
+  end
+
+  def birth_date_exists?
+    errors.add(:death_date, "birth date doesn't exist") if death_date.present? && birth_date.present? == false
+  end
 end
