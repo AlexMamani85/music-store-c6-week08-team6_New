64a9773d574f473283f2ad28ec38f02156b9af75
Enmanuel <emachipana0102@gmail.com>
2022-04-13 23:23:15 -0500

    ADD: migration for add column name in to artists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_034546) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_042039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_034546) do
     t.date "death_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "customers", force: :cascade do |t|
