59e6161ffc0e5b5c54d1260a45a96faa5a2ca619
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-04-13 20:19:18 -0500

    Add:model song

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_011710) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_011837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_011710) do
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "duration"
+    t.bigint "album_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
+  end
+
   add_foreign_key "albums", "artists"
   add_foreign_key "orders", "customers"
+  add_foreign_key "songs", "albums"
 end
