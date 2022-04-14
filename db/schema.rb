e722b6af29da8f800c1d2097d72a433fd1893f31
Enmanuel <emachipana0102@gmail.com>
2022-04-14 12:09:42 -0500

    ADD: chema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_051204) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_165535) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_051204) do
     t.string "username"
     t.string "email"
     t.string "password"
-    t.boolean "active_flag"
+    t.boolean "active_flag", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
