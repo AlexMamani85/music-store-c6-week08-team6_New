f5f6afe85a485c8cfc1ccb5c60c4680e2e869a72
Enmanuel <emachipana0102@gmail.com>
2022-04-13 22:45:37 -0500

    ADD: Validations of customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: { with: /[a-z]*@[a-z]*\.[a-z]{2,3}/ }
+    validates :password, presence: true
+    validates :password, format: { with: /[A-Za-z0-9]{6,}/ }
 end
