c1efcf6b5cf60013f426cb7f1d1baef6f20e3910
Enmanuel <emachipana0102@gmail.com>
2022-04-14 00:18:32 -0500

    Corrected with rubocop && add index unique username and email

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,15 +7,23 @@ class Artist < ApplicationRecord
   private
 
   def date_cant_be_in_the_future
-    errors.add(:birth_date, "can't be in the future") if birth_date.present? && birth_date > Date.current
+    return unless birth_date.present? && birth_date > Date.current
+
+    errors.add(:birth_date,
+               "can't be in the future")
   end
 
   def birth_date_non_greater_than_death_date
+    return unless birth_date.present? && death_date.present? && birth_date > death_date
+
     errors.add(:death_date,
-         "invalid death date must be greater than birth date") if birth_date.present? && death_date.present? && birth_date > death_date
+               "invalid death date must be greater than birth date")
   end
 
   def birth_date_exists?
-    errors.add(:death_date, "birth date doesn't exist") if death_date.present? && birth_date.present? == false
+    return unless death_date.present? && birth_date.present? == false
+
+    errors.add(:death_date,
+               "birth date doesn't exist")
   end
 end
